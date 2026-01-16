efebe65780acb1c37244e45f6feca2562e082df6
Fmartin-Soft <finley.martin-6@students.plymouth.ac.uk>
2026-01-16 15:23:26 +0000

    Added TestImage and a non-working way to get the game covers

--- Library.js
+++ Library.js
@@ -1,11 +1,13 @@
 async function SearchQueries(){
     text = document.getElementById("Search").value; //get the value of the search bar
-    data = await Apifetch(text); //wait for the Apifetch function
-    document.getElementById("TestOutput").textContent = JSON.stringify(data); //set the testoutput to the JSON but cast it first
+    data = await ApiGamefetch(text); //wait for the Apifetch function
+    imgurl = await ApiCoverfetch(data[0].cover);
+    document.getElementById("TestOutput").textContent = data[0].cover; //set the testoutput to the JSON but cast it first
+    document.getElementById("TestImage").src = imgurl[0].url;
 }
 
 //an asyncronous function of Apifetch, fetches results based on what is inputted into the seach bar
-async function Apifetch(text){
+async function ApiGamefetch(text){
     //error catching
     try{ 
     //basic API calls
@@ -14,11 +16,35 @@ async function Apifetch(text){
         //include these headers
         headers: {
             'Accept':'application/json',
-            'Client-ID': 'SECRET', //MY client ID
-            'Authorization': 'Bearer SECRET', // MY secret ID, hopefully this isnt here
+            'Client-ID': 'clientID', //MY client ID
+            'Authorization': 'Bearer ID', // MY secret ID, hopefully this isnt here
         },
 
-        body: `fields summary; search "${text}"; limit 1;` //specific search querie. Right now its searching for the summary of the game thats enterened into the search bar
+        body: `fields name,rating,rating_count,platforms,cover; search "${text}"; limit 1;` //specific search querie. Right now its searching for the summary of the game thats enterened into the search bar
+    });
+
+    data = await response.json(); //wait for the result
+    return data; // return the data
+    }catch(error){  //if error is caught
+        return error; //return the error
+    }
+}
+
+//an asyncronous function of Apifetch, fetches results based on what is inputted into the seach bar
+async function ApiCoverfetch(text){
+    //error catching
+    try{ 
+    //basic API calls
+    response = await fetch("https://corsproxy.io/?https://api.igdb.com/v4/covers",{ //fetch this: // corsproxy is used as igdb doesnt allow browser JS to call it
+        method: 'POST', // use the method of post
+        //include these headers
+        headers: {
+            'Accept':'application/json',
+            'Client-ID': 'clientID', //MY client ID
+            'Authorization': 'Bearer ID', // MY secret ID, hopefully this isnt here
+        },
+
+        body: `fields url; search "${text}"; limit 1;` //specific search querie. Right now its searching for the summary of the game thats enterened into the search bar
     });
 
     data = await response.json(); //wait for the result
