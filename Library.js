d8f2d41b9e55c6c7d0fc976113392ffb4a2bdf4c
Fmartin-Soft <finley.martin-6@students.plymouth.ac.uk>
2026-02-26 14:56:15 +0000

    Created functions for localStorage. Added plans in each

--- Library.js
+++ Library.js
@@ -1,17 +1,48 @@
+function onStart(){
+    //Check for existing localstorage
+        //if so, loadLocal()
+}
+
+function loadLocal(){
+    //check how many entries there are
+    //for each entry, 
+        //load and create boxartURL
+        //load name
+        //load platforms
+        //send into addSingleElement()
+}
+
+function createLocalEntry(URL,name,platforms){
+    //unpack URL (grab the ending string)
+    //check if localStorage exists
+        //if TRUE unpack current localStorage
+        //add new entry on end current (¦URL,name,platforms¦)
+
+        //if FALSE create new storage key
+        //add new entry (¦URL,name,platforms¦)
+
+
+}
+
+
 async function SearchQueries(){
+    gameQuery = "games/";
+    platformQuery = "platforms/";
     text = document.getElementById("Search").value; //get the value of the search bar
-    textData = await ApiGamefetch(`fields name,rating,rating_count,platforms,cover,url; search "${text}"; limit 1;`); //searching for the game plus details
-    imgurl = await ApiGamefetch(`fields cover.url; where id=${textData[0].id};`); //searching for the url cover
-    platformUrl = await ApiPlatformfetch(`fields platform_logo.url,name; where id=(${textData[0].platforms.join(",")});`)
+    textData = await IgdbApiFetch(`fields name,rating,rating_count,platforms,cover,url; search "${text}"; limit 1;`,gameQuery); //searching for the game plus details
+    imgurl = await IgdbApiFetch(`fields cover.url; where id=${textData[0].id};`,gameQuery); //searching for the url cover
+    platformUrl = await IgdbApiFetch(`fields platform_logo.url,name; where id=(${textData[0].platforms.join(",")});`,platformQuery)
     await addSingleElement(textData[0].name,platformUrl,"https:" + imgurl[0].cover.url.replace("t_thumb","t_cover_big"));
+    
+    //createlocalEntry()
 }
 
 //an asyncronous function of Apifetch, fetches results based on what is inputted into the seach bar
-async function ApiGamefetch(body){
+async function IgdbApiFetch(body,Query){
     //error catching
     try{ 
     //basic API calls
-    response = await fetch("https://corsproxy.io/?https://api.igdb.com/v4/games/",{ //fetch this: // corsproxy is used as igdb doesnt allow browser JS to call it
+    response = await fetch(`https://corsproxy.io/?https://api.igdb.com/v4/${Query}`,{ //fetch this: // corsproxy is used as igdb doesnt allow browser JS to call it
         method: 'POST', // use the method of post
         //include these headers
         headers: {
@@ -29,34 +60,6 @@ async function ApiGamefetch(body){
         return error; //return the error
     }
 }
-
-//an asyncronous function of Apifetch, fetches results based on what is inputted into the seach bar
-async function ApiPlatformfetch(body){
-    //error catching
-    try{ 
-    //basic API calls
-    response = await fetch("https://corsproxy.io/?https://api.igdb.com/v4/platforms/",{ //fetch this: // corsproxy is used as igdb doesnt allow browser JS to call it
-        method: 'POST', // use the method of post
-        //include these headers
-        headers: {
-            'Accept':'application/json',
-            'Client-ID': 'ID', //MY client ID
-            'Authorization': 'Bearer ID', // MY secret ID, hopefully this isnt here
-        },
-
-        body: `${body}` //dynamic search body.
-    });
-
-    data = await response.json(); //wait for the result
-    return data; // return the data
-    }catch(error){  //if error is caught
-        return error; //return the error
-    }
-}
-
-
-
-
 
 
 //event listenener for keypresses
